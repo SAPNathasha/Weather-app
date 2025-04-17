4841f1d87552526b2e92b703ca2cf682348bdab7
SAPNathasha <131260315+SAPNathasha@users.noreply.github.com>
2025-04-17 10:51:42 +0530

    Update app.js by setting API key url to the system

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
 const express = require("express");
 const app = express();
+const https = require("https");
+
 
 app.get("/" , function(req, res){
+    const url = "https://api.openweathermap.org/data/2.5/weather?q=London&appid=a1c54c1086724e47f8f8a4be0594d518&units=metric";
+    https.get(url, function(response){
+        console.log(response);
+    })
     res.send("Server is up and running.");
 })
 
