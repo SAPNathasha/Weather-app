5cde8a87232df521735ab28a9d96d8fa4d5c591b
SAPNathasha <131260315+SAPNathasha@users.noreply.github.com>
2025-04-17 13:04:53 +0530

    Get status codes using "statusCode"

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const https = require("https");
 app.get("/" , function(req, res){
     const url = "https://api.openweathermap.org/data/2.5/weather?q=London&appid=a1c54c1086724e47f8f8a4be0594d518&units=metric";
     https.get(url, function(response){
-        console.log(response);
+        console.log(response.statusCode);
     })
     res.send("Server is up and running.");
 })
